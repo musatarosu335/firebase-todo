e7f2b38db4102bc5814ff81dc28264d5612aeab7
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-20 14:11:34 +0900

    functions作成

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors')({ origin: true });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+// Expressのインスタンス化と、CORSモジュールのロード
+const app = express();
+app.use(cors);
+
+/* ==================================
+RESTful API
+================================== */
+// Todo一覧取得
+app.get('/todos', (req, res) => {
+  const db = admin.firestore();
+  const todos = [];
+
+  db.collection('todos').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        const todo = doc.data();
+        todos.push(todo);
+      });
+      res.header('Content-Type', 'application/json; charset=utf-8');
+      res.send({ todos });
+    })
+    .catch((err) => {
+      console.log(err); // eslint-disable-line no-console
+      res.send(err);
+    });
+});
+
+// RESTful API を利用
+exports.v1 = functions.https.onRequest(app);
